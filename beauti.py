c2515b8eeca4ee56202c73c6a4e31904fbad6230
nerel <gorzkiewicz.artur97@gmail.com>
2023-03-24 12:08:16 +0100

    beauti

---
+++ beauti.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+
+html = '''
+<h1>Green turtle</h1>
+<p>the turtle is patient</p>
+'''
+
+keyword = 'green'
+
+soup = BeautifulSoup(html, 'html.parser')
+
+for tag in soup.find_all():
+	if tag.string and keyword.lower() in tag.string.lower():
+		tag.decompose()
+		html = str(soup)
+		print(html)
